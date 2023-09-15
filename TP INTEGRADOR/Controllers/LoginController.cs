a471b88cb10c77cfc7147979b053ea35c85ab720
mxv-0827 <maximilianoviand@gmail.com>
2023-09-15 18:16:11 -0300

    JWT implementado.

---
+++ TP INTEGRADOR/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TP_INTEGRADOR.DTOs;
+using TP_INTEGRADOR.Helpers;
+using TP_INTEGRADOR.Services;
+
+namespace TP_INTEGRADOR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private JWT_TokenHelper _token;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LoginController(IUnitOfWork unitOfWork, IConfiguration configuration)
+        {
+            _unitOfWork = unitOfWork;
+            _token = new JWT_TokenHelper(configuration);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AuthenticateDTO userToAuthenticate)
+        {
+            var userCredentials = await _unitOfWork.UserRepository.AuthenticateCredentials(userToAuthenticate);
+
+            if (userCredentials is null) return Unauthorized("Incorrect combination of credentials.");
+
+            var token = _token.GenerateToken(userCredentials);
+
+            var userAuthenticated = new LoginDTO
+            {
+                UserName = userCredentials.Name,
+                UserDNI = userCredentials.DNI,
+                UserToken = token
+            };
+
+            return Ok(userAuthenticated);
+        }
+    }
+}
---
+++ TP INTEGRADOR/Helpers/JWT_TokenHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TP_INTEGRADOR.Entities;
+
+namespace TP_INTEGRADOR.Helpers
+{
+    public class JWT_TokenHelper
+    {
+        private IConfiguration _configuration;
+
+        public JWT_TokenHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["JWT:Subject"]),
+                new Claim(ClaimTypes.NameIdentifier, user.CodUser.ToString()),
+                new Claim(ClaimTypes.Role, user.UserRole.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddHours(8), signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
